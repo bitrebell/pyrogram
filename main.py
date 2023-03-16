22ac189fe5cadfa8cc1d79602f17edb3a243ce6d
Darkranger <darkanger008@gmail.com>
2023-03-16 23:40:56 +0530

    new arrival

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pyrogram import Client
+
+bot = Client(
+    "My first Project",
+    api_id = "10738943",
+    api_hash = "da61e3a08b5ac78ce28b4a4cd854aeec",
+    bot_token = "5974422696:AAEGU0ZYnhgrNeNlpf_Nw8dPbbt00Wmctpc"
+)
+
+@bot.on_message(filters.command('start') & filters.private)
+def command1(bot, message):
+    bot.send_message(message.chat.id, "Heya, I am  a simple testing Bot")
+
+bot.run()
 
